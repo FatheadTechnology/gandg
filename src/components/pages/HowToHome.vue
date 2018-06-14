31573c19b9305689773b892c88ca10f8c43a9685
Jacob Shutty <jacobshutty@fathead.com>
2018-06-14 16:34:19 -0400

    Adds styling for how to pages and blog pages

--- src/components/pages/HowToHome.vue
+++ src/components/pages/HowToHome.vue
@@ -1,65 +1,21 @@
 <template>
-  <div id="home">
-    <div id="hero">
-      <div id="hero-content">
-        <h2>Transform Your Room</h2>
-        <p>
-          Our wallpaper is easy to hang and comes off clean so you can make your room match your style without the fuss. </p>
-        <div id="hero-btn" class="btn primary-btn">CALL TO ACTION</div>
-      </div>
+  <div id="how-to-home">
+    <h1>How To</h1>
+    <p>Watch the video or follow our step-by-step instructions of how to apply and remove your removable wallpaper.</p>
+    <div class="video-wrap">
+      <iframe width="560" height="315" src="https://www.youtube.com/embed/_Lvp6Gl9lUY?rel=0&amp;showinfo=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
     </div>
-    <div id="cta-hero-bar">
-      Memorial Day Sale: 30% Off Select Styles
-
-
-
-    </div>
-
-    <div id="subheader-grid-container">
-      <div class="product-grid">
-        <div class="product-grid-item-container">
-          <div class="product-grid-item-image-container">
-            <div class="product-grid-item-image"  style="background-image: url('https://assets.fathead.com/swipe-patterns/HeatherDuttonLamaisondespapillonsNavy12x12.jpg');"></div>
-          </div>
-          <span class="product-grid-item-header"> vintage</span>
-          <span class="product-grid-item-subheader">gritty & grungy</span>
-        </div>
-
-        <div class="product-grid-item-container">
-          <div class="product-grid-item-image-container">
-            <div class="product-grid-item-image"  style="background-image: url('https://assets.fathead.com/swipe-patterns/HeatherDuttonDunesGray24x24.jpg');"></div>
-          </div>
-          <span class="product-grid-item-header"> Geometric</span>
-          <span class="product-grid-item-subheader">patterns & lines</span>
-        </div>
-
-        <div class="product-grid-item-container">
-          <div class="product-grid-item-image-container">
-            <div class="product-grid-item-image"  style="background-image: url('https://assets.fathead.com/swipe-patterns/resting_sparrows_white.jpg');"></div>
-          </div>
-          <span class="product-grid-item-header"> Damask</span>
-          <span class="product-grid-item-subheader">traditional style</span>
-        </div>
-
-        <div class="product-grid-item-container">
-          <div class="product-grid-item-image-container">
-            <div class="product-grid-item-image" style="background-image: url('https://assets.fathead.com/swipe-patterns/deco_diamond_blue-01.jpg');"></div>
-          </div>
-          <span class="product-grid-item-header">Kids</span>
-          <span class="product-grid-item-subheader">fun & funky</span>
-        </div>
-      </div>
-    </div>
-
+    <h1>How to Hang Removable Wallpaper</h1>
+    <p>Lorem ipsum dolor sit amet, eros mentitum duo ea. Labitur officiis et sed. Soluta maiorum an sea. Eum nuhil persius te. Impetus nesessitatibus id, cu inermis assueverit est.</p>
     <div class="btn-center-container">
-      <div class="btn primary-btn">See all design styles</div>
+      <div class="btn primary-btn">Step-by-Step Instructions</div>
     </div>
-
-    <h2>Featured Patterns</h2>
-    <product-grid-line-mock></product-grid-line-mock>
-    <div class="btn-center-container">
-      <div class="btn primary-btn">See all patterns</div>
-    </div>
+    <h1>More How-to Videos</h1>
+    <video-grid></video-grid>
+    <video-grid></video-grid>
+    <br>
+    <br>
+    <br>
 
     <h2>Recently Viewed</h2>
     <product-grid-line-mock></product-grid-line-mock>
@@ -67,75 +23,28 @@
     <div class="btn-center-container">
       <div class="btn primary-btn">See all viewed patterns</div>
     </div>
-
-
-    <div id="featured-artist-grid-container">
-      <h1>Featured Artist</h1>
-      <p>Multi-disciplinary designers, Wayne & Gerardine Hemingway, bring an iconic style to their wallpaper designs
-with a graphic style & vintage cool.</p>
-      <div class="product-grid">
-
-        <div class="product-grid-item-container">
-          <div class="product-grid-item-image-container">
-            <div class="product-grid-item-image" style="background-image: url('https://assets.fathead.com/swipe-patterns/deco_diamond_blue-01.jpg');"></div>
-          </div>
-          <span class="product-grid-item-header"> Some Product Name</span>
-          <span class="product-grid-item-subheader">6 colors available</span>
-        </div>
-
-        <div class="product-grid-item-container">
-          <div class="product-grid-item-image-container">
-            <div class="product-grid-item-image"  style="background-image: url('https://assets.fathead.com/swipe-patterns/resting_sparrows_white.jpg');"></div>
-          </div>
-          <span class="product-grid-item-header"> Some Product Name</span>
-          <span class="product-grid-item-subheader">6 colors available</span>
-        </div>
-
-        <div class="product-grid-item-container">
-          <div class="product-grid-item-image-container">
-            <div class="product-grid-item-image"  style="background-image: url('https://assets.fathead.com/swipe-patterns/HeatherDuttonDunesGray24x24.jpg');"></div>
-          </div>
-          <span class="product-grid-item-header"> Some Product Name</span>
-          <span class="product-grid-item-subheader">6 colors available</span>
-        </div>
-
-        <div class="product-grid-item-container">
-          <div class="product-grid-item-image-container">
-            <div class="product-grid-item-image"  style="background-image: url('https://assets.fathead.com/swipe-patterns/HeatherDuttonLamaisondespapillonsNavy12x12.jpg');"></div>
-          </div>
-          <span class="product-grid-item-header"> Some Product Name</span>
-          <span class="product-grid-item-subheader">6 colors available</span>
-        </div>
-      </div>
-
-      <div class="btn-center-container">
-        <div class="btn primary-btn">SEE THE COLLECTION</div>
-        <a href="#">See All Artists</a>
-      </div>
-
-    </div>
   </div>
-
 
 </template>
 
 <script>
-  import ProductGridLineMock from '../ProductGridLineMock'
+import ProductGridLineMock from "../ProductGridLineMock";
+import VideoGrid from "../VideoGrid";
 
-  export default {
-    name: 'HelloWorld',
-    components: {
-      ProductGridLineMock
-    },
-    data () {
-      return {
-        msg: 'Welcome to Your Vue.js App'
-      }
-    }
+export default {
+  name: "HelloWorld",
+  components: {
+    ProductGridLineMock,
+    VideoGrid
+  },
+  data() {
+    return {
+      msg: "Welcome to Your Vue.js App"
+    };
   }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
 </style>
